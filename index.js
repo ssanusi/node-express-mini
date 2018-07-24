81fe088b9800f7ca371bf8a4f7cec85cf92ed6d2
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2018-07-24 08:19:59 +0100

    refactor get request

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const db = require('./data/db')
 const app = express()
 
-app.get('/', (req, res)=>{
-  res.send('Hello its working')
-})
-
 app.get('/api/users', (req, res) => {
   db.find()
         .then(users => {
-          res.json(users)
+          if(users.length === 0){
+            res.status(404).json({ message : `No users available` })
+          }else {
+            res.json(users)
+          }
         })
         .catch(err => {
           console.log(`we got the error ${err} from server `)
@@ -20,10 +20,15 @@ app.get('/api/users/:id', (req, res) => {
   const { id } = req.params
   db.findById(id)
         .then(user => {
-          res.json(user)
+          if(user.length === 0){
+            res.status(404).json({message : `user not found`})
+          }
+          else {
+            res.status(200).json(user)
+          }
         })
         .catch(err => {
-          console.log(`we got the error ${err} from server `)
+          res.status(400).json({ error : `the server return ${err}`  })
         })
 } )
 
